34da7a086804b0e5dda3621d2ee5edb4083029b4
ddabo <ddabo@tvz.hr>
2017-03-22 14:53:29 +0100

    Added comment to SecondActivity for merge testing.

--- app/src/main/java/hr/dabo/ravnoizzagreba/SecondActivity.java
+++ app/src/main/java/hr/dabo/ravnoizzagreba/SecondActivity.java
@@ -10,4 +10,6 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
     }
+
+    /* Comment made for merge testing*/
 }
