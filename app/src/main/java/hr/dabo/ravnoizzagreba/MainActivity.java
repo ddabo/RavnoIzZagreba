d3cab31eb320b68bb9892e05e6758453e95d835f
ddabo <ddabo@tvz.hr>
2017-03-22 11:54:59 +0100

    First commit for testing. Modified MainActivity and added new Activity called Second. Also some changes to the 'strings.xml' folder are done in the process.

--- app/src/main/java/hr/dabo/ravnoizzagreba/MainActivity.java
+++ app/src/main/java/hr/dabo/ravnoizzagreba/MainActivity.java
@@ -2,12 +2,18 @@ package hr.dabo.ravnoizzagreba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewHelloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewHelloWorld = (TextView) findViewById(R.id.text_hello_world);
+        textViewHelloWorld.setText(getResources().getText(R.string.welcome));
     }
 }
